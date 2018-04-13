65ed2532acdb06a41d6f4e16bf1871dba639fee2
NikoJingyuHuang <38271309+NikoJingyuHuang@users.noreply.github.com>
2018-04-13 14:50:51 +0800

    whole things

---
+++ Assets/Scripts/Level/doorrise.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class doorrise : MonoBehaviour
+{
+
+    public float updistance = 0;
+    
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+   
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+
+        if (collision.collider.tag != "LightOrb") return;
+        transform.Translate(0, updistance, 0);
+
+    }
+    
+}
